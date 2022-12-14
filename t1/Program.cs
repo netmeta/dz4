92007e3f09bdd13de066d3aa5268eb146c8d790c
netmeta <moron686@gmail.com>
2022-12-15 00:39:38 +0300

    1

---
+++ t1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Не использовать функцию Math.Pow
+// 3, 5 -> 243 (3⁵)
+// 2, 4-> 16
+
+Console.Write("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+int result = a;
+
+for (int i = 1; i < b; i++)
+{
+    result *= a;
+}
+Console.Write(result);
---
+++ t2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму чисел, являющиеся делителями этого числа !
+// 452 -> 4+2 = 6
+// 82 -> 2
+// 9012 -> 3
+
+
+Console.Write("Введите число : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Sumofnumber(int number)
+{
+    int sum = 0;
+
+    for (int i = number; i > 0; i /= 10)
+    {
+        if (((i % 10) == 0)||(number % (i % 10) == 0))
+        {
+            sum += (i % 10);
+        }
+    }
+
+    return sum;
+}
+
+int result = Sumofnumber(n);
+Console.WriteLine(result);
